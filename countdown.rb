f36c3edfa35bfc3e9c9f4b999053238ebb593467
ahmad sulasi <ahmad.luthfi@student.upi.edu>
2017-03-13 10:09:09 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(input)
+  a=input
+  while a !=0
+    puts "#{a} SECOND(S)!"
+    a=a-1
+  end
+  return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(angka)
+  a=angka
+  while a !=0
+    puts "#{a} SECOND(S)!"
+    a=a-1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
