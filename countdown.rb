b56d31a18e71e85c389953180f82374d58bf8ee6
Esphas Kueen <esphas@hotmail.com>
2017-06-23 02:25:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown tt
+  tt.times do |t| puts "#{tt-t} SECOND(S)!" end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep tt
+  tt.times do |t| puts "#{tt-t} SECOND(S)!"; sleep 1 end
+  "HAPPY NEW YEAR!"
 end
