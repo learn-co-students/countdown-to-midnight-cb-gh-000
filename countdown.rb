78b88746d6062e95899e3f88818a93a40bde5dd4
Leslie Huang <lesliehuang@gmail.com>
2017-06-27 18:56:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  while num > 0
+    puts "#{num} SECOND(S)"
+    num -= 1
+  end
+end
+
+def coundtown_with_sleep(num)
+  until num == 0
+    puts "#{num} SECOND(S)"
+    num -= 1
+    sleep(2)
+  end
 end
