c249dd22c5abe0bb6fc46bf9470941a45658cbc7
William Jang <wjang20@amherst.edu>
2017-08-26 20:31:45 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    countdown_with_sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(sec)
+  sleep(sec)
 end
