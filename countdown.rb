1a7643f1d84aad63ac135ecd94a16a83349f2215
Yi Xuan Sim <yxuan1996@gmail.com>
2017-08-29 14:56:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  lumber = number.to_i
+  while lumber > 0
+    puts "#{lumber} SECOND(S)!"
+    lumber -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  lumber = number.to_i
+  while lumber > 0
+    puts "#{lumber} SECOND(S)!"
+    sleep(1)
+    lumber -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
