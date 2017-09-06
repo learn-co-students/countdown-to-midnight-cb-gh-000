99c604407e7e6ae099b221c09b6b2d9ca56c8845
Colin McKean <ezikill@hotmail.com>
2017-09-06 14:49:20 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown seconds
+  seconds.times do
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep seconds
+  sleep(seconds)
+  seconds.times do
+    puts "#{seconds} seconds"
+  end
 end
