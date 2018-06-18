b9d055caf1817e3fafc2d032deb93d549395da39
Krishan Jethwa <krishan.jethwa@student.manchester.ac.uk>
2018-06-18 16:03:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  if int == 0
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  if int == 0
+    return "HAPPY NEW YEAR!"
+  end
 end
